b044cb70e764caba23175701607ecfa8a29f07bb
wcyuan <wcyuan@users.noreply.github.com>
2019-08-21 18:19:30 -0400

    allow changing the width and number of pegs

--- src/index.tsx
+++ src/index.tsx
@@ -16,7 +16,7 @@ function Peg(props) {
            className={className}
            onClick={props.onClick}
            disabled={props.disabled}
-           style={style} />);
+           style={style} >{props.text}</button>);
 }
 
 function EvalPeg(props) {
@@ -29,11 +29,12 @@ function GuessPeg(props) {
     onClick: props.onClick,
     size: "biggest",
     disabled:  false,
+    text: props.text,
   });
 }
 
 function HistoryPeg(props) {
-  return Peg({color: props.color, size: "big"});
+  return Peg({color: props.color, text: props.text, size: "big"});
 }
 
 
@@ -43,6 +44,7 @@ function Row(props) {
     items.push(
       <HistoryPeg
        key={ii}
+       text={props.guess[ii]}
        color={Util.getColor(props.game, props.guess[ii])} />)
   }
   for (let ii = 0; ii < props.result.exact; ii++) {
@@ -64,7 +66,9 @@ class NextGuessRow extends Component {
       this.state.guess = props.guess;
     } else {
       for (let ii = 0; ii < this.props.game.width; ii++) {
-        this.state.guess.push(this.props.game.valid_values[ii]);
+        this.state.guess.push(
+          this.props.game.valid_values[
+            ii%this.props.game.valid_values.length]);
       }
     }
   }
@@ -87,6 +91,7 @@ class NextGuessRow extends Component {
       items.push(
         <GuessPeg
          key={ii}
+         text={this.state.guess[ii]}
          color={
            Util.getColor(this.props.game, this.state.guess[ii])}
          onClick={() => this.handleClick(ii)} 
@@ -114,7 +119,9 @@ class Util {
   }
   //  https://stackoverflow.com/a/7419630
   static rainbow(numOfSteps, step) {
-      // This function generates vibrant, "evenly spaced" colours (i.e. no clustering). This is ideal for creating easily distinguishable vibrant markers in Google Maps and other apps.
+      // This function generates vibrant, "evenly spaced" colours (i.e. no clustering).
+      // This is ideal for creating easily distinguishable vibrant markers in
+      // Google Maps and other apps.
       // Adam Cole, 2011-Sept-14
       // HSV to RBG adapted from: http://mjijackson.com/2008/02/rgb-to-hsl-and-rgb-to-hsv-color-model-conversion-algorithms-in-javascript
       var r, g, b;
@@ -135,25 +142,62 @@ class Util {
   }
 }
 
+function EntryBox(props) {
+     return (
+        <input
+         type="text"
+         value={props.value}
+         onChange={props.onChange}
+         size={2}
+       />
+     );
+}
+
 class GameUI extends Component {
   constructor(props) {
     super(props);
-    let valid_values = [];
-    for (let ii = 0; ii < props.num_pegs; ii++) {
-      valid_values.push("" + ii);
-    }
-    const game = new mastermind.Game(props.width, valid_values);
+    const game = this.makeGame(props.numColors, props.width);
     this.state = {
+      numColors: props.numColors,
+      width: props.width,
       game: game,
       secret: game.get_random_guess(),
       history: [],
       auto: new mastermind.AutoPlayer(game),
       random: new mastermind.RandomPlayer(game),
-      // Set a key to make it easy to reset components
+      firstguess: null,
+      // Set a key to make it easy to reset components	
       // https://stackoverflow.com/a/21750576
       key: new Date(),
-      firstguess: null,
     };
+  }
+  makeGame(numColors, width) {
+    let valid_values = [];
+    for (let ii = 0; ii < numColors; ii++) {
+      valid_values.push("" + ii);
+    }
+    return new mastermind.Game(width, valid_values);    
+  }
+  resetState(numColors, width) {
+    const game = this.makeGame(numColors, width);
+    this.setState({
+      game: game,
+      width: width,
+      numColors: numColors,
+      secret: game.get_random_guess(),
+      history: [],
+      auto: new mastermind.AutoPlayer(game),
+      random: new mastermind.RandomPlayer(game),
+      firstguess: null,
+      key: new Date(),
+    });
+  }
+  setWidth(event) {
+    this.resetState(this.state.numColors, event.target.value);
+  }
+
+  setNumColors(event) {
+    this.resetState(event.target.value, this.state.width);  
   }
   updateHistory(game, auto, secret, guess, history) {
     let result = game.check_guess(
@@ -180,6 +224,7 @@ class GameUI extends Component {
       secret: this.state.game.get_random_guess(),
       history: [],
       key: new Date(),
+      firstguess: null,
     });
   }
   newOneMoveGame() {
@@ -222,6 +267,14 @@ class GameUI extends Component {
     }
     return (
       <div key={this.state.key}>
+
+      <p>
+      Colors:
+      <EntryBox value={this.state.numColors} onChange={this.setNumColors.bind(this)}/> 
+      Width:
+      <EntryBox value={this.state.width} onChange={this.setWidth.bind(this)}/> 
+      </p>
+
       <button onClick={() => this.newGame()}>
        New Game
       </button>
@@ -232,7 +285,7 @@ class GameUI extends Component {
       {past_guesses}
       <hr/>
       <NextGuessRow
-       key="next"
+       key={this.state.key + "_next"}
        game={this.state.game}
        auto={this.getAutoGuess.bind(this)}
        onSubmit={this.handleNewGuess.bind(this)}     
@@ -247,15 +300,14 @@ class App extends Component<AppProps, AppState> {
   constructor(props) {
     super(props);
     this.state = {
-      name: 'React'
+      name: 'React',
     };
   }
-
   render() {
     return (
       <div>
         <h1>Mastermind!</h1>
-        <GameUI width={4} num_pegs={6} />
+        <GameUI width={4} numColors={6} />
       </div>
     );
   }
