b49e0210209439c8de13921f925e0451bd07b16a
wcyuan <wcyuan@users.noreply.github.com>
2019-08-21 23:20:58 -0400

    change == to ===
    recommendations from codesandbox.io

--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,30 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
-import './style.css';
+import React, { Component } from "react";
+import { render } from "react-dom";
+import "./style.css";
 import * as mastermind from "./mastermind";
 
-interface AppProps { }
+interface AppProps {}
 interface AppState {
   name: string;
 }
 
 function Peg(props) {
-  const style = {backgroundColor: props.color};
-  const className = [props.size, "peg"].join(' ');
-  const  onclick = "";
-  return (<button
-           className={className}
-           onClick={props.onClick}
-           disabled={props.disabled}
-           style={style} >{props.text}</button>);
+  const style = { backgroundColor: props.color };
+  const className = [props.size, "peg"].join(" ");
+  return (
+    <button
+      className={className}
+      onClick={props.onClick}
+      disabled={props.disabled}
+      style={style}
+    >
+      {props.text}
+    </button>
+  );
 }
 
 function EvalPeg(props) {
-  return Peg({color: props.color, size: "small", disabled: true});
+  return Peg({ color: props.color, size: "small", disabled: true });
 }
 
 function GuessPeg(props) {
@@ -28,32 +32,37 @@ function GuessPeg(props) {
     color: props.color,
     onClick: props.onClick,
     size: "biggest",
-    disabled:  false,
-    text: props.text,
+    disabled: false,
+    text: props.text
   });
 }
 
 function HistoryPeg(props) {
-  return Peg({color: props.color, text: props.text, size: "big"});
+  return Peg({ color: props.color, text: props.text, size: "big" });
 }
-
 
 function Row(props) {
   const items = [];
   for (let ii = 0; ii < props.guess.length; ii++) {
     items.push(
       <HistoryPeg
-       key={ii}
-       text={props.guess[ii]}
-       color={Util.getColor(props.game, props.guess[ii])} />)
+        key={ii}
+        text={props.guess[ii]}
+        color={Util.getColor(props.game, props.guess[ii])}
+      />
+    );
   }
   for (let ii = 0; ii < props.result.exact; ii++) {
-    items.push(<EvalPeg color="red" key={"exact"+ii} />)
+    items.push(<EvalPeg color="red" key={"exact" + ii} />);
   }
   for (let ii = 0; ii < props.result.partial; ii++) {
-    items.push(<EvalPeg color="white" key={"partial"+ii} />)
+    items.push(<EvalPeg color="white" key={"partial" + ii} />);
   }
-  return (<div>{items} ({props.num_left})</div>);
+  return (
+    <div>
+      {items} ({props.num_left})
+    </div>
+  );
 }
 
 class NextGuessRow extends Component {
@@ -61,28 +70,28 @@ class NextGuessRow extends Component {
     super(props);
     this.state = {
       guess: []
-    }
+    };
     if ("guess" in props && props.guess != null) {
       this.state.guess = props.guess;
     } else {
       for (let ii = 0; ii < this.props.game.width; ii++) {
         this.state.guess.push(
-          this.props.game.valid_values[
-            ii % this.props.game.valid_values.length]);
+          this.props.game.valid_values[ii % this.props.game.valid_values.length]
+        );
       }
     }
   }
-  handleClick(ii:number) {
+  handleClick(ii: number) {
     let guess = this.state.guess.slice();
     const value = guess[ii];
     let index = this.props.game.valid_values.indexOf(value);
     index = (index + 1) % this.props.game.valid_values.length;
     guess[ii] = this.props.game.valid_values[index];
-    this.setState({guess: guess});
+    this.setState({ guess: guess });
   }
   makeAutoGuess() {
     const guess = this.props.auto();
-    this.setState({guess: guess});
+    this.setState({ guess: guess });
     this.props.onSubmit(guess);
   }
   render() {
@@ -90,68 +99,95 @@ class NextGuessRow extends Component {
     for (let ii = 0; ii < this.props.game.width; ii++) {
       items.push(
         <GuessPeg
-         key={ii}
-         text={this.state.guess[ii]}
-         color={
-           Util.getColor(this.props.game, this.state.guess[ii])}
-         onClick={() => this.handleClick(ii)} 
-        />);
+          key={ii}
+          text={this.state.guess[ii]}
+          color={Util.getColor(this.props.game, this.state.guess[ii])}
+          onClick={() => this.handleClick(ii)}
+        />
+      );
     }
     return (
       <div>
-      {items}
-      <button onClick={() => this.props.onSubmit(this.state.guess)}>
-       Guess
-      </button>
-      <button onClick={() => this.makeAutoGuess()}>
-       Auto Guess
-      </button>
+        {items}
+        <button onClick={() => this.props.onSubmit(this.state.guess)}>
+          Guess
+        </button>
+        <button onClick={() => this.makeAutoGuess()}>Auto Guess</button>
       </div>
-      );
+    );
   }
 }
 
 class Util {
   static getColor(game, value) {
     return Util.rainbow(
-      game.valid_values.length, 
-      game.valid_values.indexOf(value));
+      game.valid_values.length,
+      game.valid_values.indexOf(value)
+    );
   }
   //  https://stackoverflow.com/a/7419630
   static rainbow(numOfSteps, step) {
-      // This function generates vibrant, "evenly spaced" colours (i.e. no clustering).
-      // This is ideal for creating easily distinguishable vibrant markers in
-      // Google Maps and other apps.
-      // Adam Cole, 2011-Sept-14
-      // HSV to RBG adapted from: http://mjijackson.com/2008/02/rgb-to-hsl-and-rgb-to-hsv-color-model-conversion-algorithms-in-javascript
-      var r, g, b;
-      var h = step / numOfSteps;
-      var i = ~~(h * 6);
-      var f = h * 6 - i;
-      var q = 1 - f;
-      switch(i % 6){
-          case 0: r = 1; g = f; b = 0; break;
-          case 1: r = q; g = 1; b = 0; break;
-          case 2: r = 0; g = 1; b = f; break;
-          case 3: r = 0; g = q; b = 1; break;
-          case 4: r = f; g = 0; b = 1; break;
-          case 5: r = 1; g = 0; b = q; break;
-      }
-      var c = "#" + ("00" + (~ ~(r * 255)).toString(16)).slice(-2) + ("00" + (~ ~(g * 255)).toString(16)).slice(-2) + ("00" + (~ ~(b * 255)).toString(16)).slice(-2);
-      return (c);
+    // This function generates vibrant, "evenly spaced" colours (i.e. no clustering).
+    // This is ideal for creating easily distinguishable vibrant markers in
+    // Google Maps and other apps.
+    // Adam Cole, 2011-Sept-14
+    // HSV to RBG adapted from: http://mjijackson.com/2008/02/rgb-to-hsl-and-rgb-to-hsv-color-model-conversion-algorithms-in-javascript
+    var r, g, b;
+    var h = step / numOfSteps;
+    var i = ~~(h * 6);
+    var f = h * 6 - i;
+    var q = 1 - f;
+    switch (i % 6) {
+      case 0:
+        r = 1;
+        g = f;
+        b = 0;
+        break;
+      case 1:
+        r = q;
+        g = 1;
+        b = 0;
+        break;
+      case 2:
+        r = 0;
+        g = 1;
+        b = f;
+        break;
+      case 3:
+        r = 0;
+        g = q;
+        b = 1;
+        break;
+      case 4:
+        r = f;
+        g = 0;
+        b = 1;
+        break;
+      case 5:
+        r = 1;
+        g = 0;
+        b = q;
+        break;
+    }
+    var c =
+      "#" +
+      ("00" + (~~(r * 255)).toString(16)).slice(-2) +
+      ("00" + (~~(g * 255)).toString(16)).slice(-2) +
+      ("00" + (~~(b * 255)).toString(16)).slice(-2);
+    return c;
   }
 }
 
 function EntryBox(props) {
-    return (
-      <input
-        type="number"
-        value={props.value}
-        onChange={props.onChange}
-        onFocus={(event) => event.target.select()}
-        style={{width:"25px"}}
-      />
-    );
+  return (
+    <input
+      type="number"
+      value={props.value}
+      onChange={props.onChange}
+      onFocus={event => event.target.select()}
+      style={{ width: "25px" }}
+    />
+  );
 }
 
 class GameUI extends Component {
@@ -164,7 +200,7 @@ class GameUI extends Component {
     for (let ii = 0; ii < numColors; ii++) {
       valid_values.push("" + ii);
     }
-    return new mastermind.Game(width, valid_values);    
+    return new mastermind.Game(width, valid_values);
   }
   getNewState(numColors, width) {
     const game = this.makeGame(numColors, width);
@@ -176,32 +212,29 @@ class GameUI extends Component {
       history: [],
       auto: new mastermind.AutoPlayer(game),
       firstguess: null,
-      // Set a key to make it easy to reset components	
+      // Set a key to make it easy to reset components
       // https://stackoverflow.com/a/21750576
-      key: new Date(),
+      key: new Date()
     };
   }
   setWidth(event) {
-    this.setState(
-      this.getNewState(
-        this.state.numColors, event.target.value));
+    this.setState(this.getNewState(this.state.numColors, event.target.value));
   }
   setNumColors(event) {
-    this.setState(
-      this.getNewState(
-        event.target.value, this.state.width));  
+    this.setState(this.getNewState(event.target.value, this.state.width));
   }
   updateHistory(game, auto, secret, guess, history, force_num_left) {
-    let result = game.check_guess(
-      guess, secret);
+    let result = game.check_guess(guess, secret);
     history.push([guess, result]);
     let num_left = "";
-    if (force_num_left ||
-        auto.is_possible_guesses_computed(history) || 
-        Math.pow(game.valid_values.length, game.width) < 1e7) {
+    if (
+      force_num_left ||
+      auto.is_possible_guesses_computed(history) ||
+      Math.pow(game.valid_values.length, game.width) < 1e7
+    ) {
       num_left = auto.get_num_possible_guesses(history);
     }
-    history[history.length-1].push(num_left);
+    history[history.length - 1].push(num_left);
     return history;
   }
   handleNewGuess(guess) {
@@ -212,7 +245,9 @@ class GameUI extends Component {
         this.state.secret,
         guess,
         this.state.history.slice(),
-        false)});
+        false
+      )
+    });
   }
   getAutoGuess() {
     return this.state.auto.make_guess(this.state.history);
@@ -222,15 +257,15 @@ class GameUI extends Component {
       secret: this.state.game.get_random_guess(),
       history: [],
       key: new Date(),
-      firstguess: null,
+      firstguess: null
     });
   }
   newOneMoveGame() {
-    while(true) {
+    while (true) {
       const secret = this.state.game.get_random_guess();
       const history = [];
       let num_left = 2;
-      while(num_left > 1) {
+      while (num_left > 1) {
         const guess = this.state.auto.make_random_guess(history);
         this.updateHistory(
           this.state.game,
@@ -238,16 +273,17 @@ class GameUI extends Component {
           secret,
           guess,
           history,
-          true);
-        num_left = history[history.length-1][2];
+          true
+        );
+        num_left = history[history.length - 1][2];
       }
-      if (history[history.length-1][1].exact !=  this.state.game.width) {
+      if (history[history.length - 1][1].exact !== this.state.game.width) {
         this.setState({
           secret: secret,
           history: history,
           key: new Date(),
-          firstguess: history[history.length-1][0],
-        })
+          firstguess: history[history.length - 1][0]
+        });
         return;
       }
     }
@@ -257,39 +293,41 @@ class GameUI extends Component {
     for (let ii = 0; ii < this.state.history.length; ii++) {
       past_guesses.push(
         <Row
-         key={ii}
-         game={this.state.game}
-         guess={this.state.history[ii][0]}
-         result={this.state.history[ii][1]}
-         num_left={this.state.history[ii][2]} />
+          key={ii}
+          game={this.state.game}
+          guess={this.state.history[ii][0]}
+          result={this.state.history[ii][1]}
+          num_left={this.state.history[ii][2]}
+        />
       );
     }
     return (
       <div key={this.state.key}>
+        <p>
+          Colors:
+          <EntryBox
+            value={this.state.numColors}
+            onChange={this.setNumColors.bind(this)}
+          />
+          Width:
+          <EntryBox
+            value={this.state.width}
+            onChange={this.setWidth.bind(this)}
+          />
+        </p>
 
-      <p>
-      Colors:
-      <EntryBox value={this.state.numColors} onChange={this.setNumColors.bind(this)}/> 
-      Width:
-      <EntryBox value={this.state.width} onChange={this.setWidth.bind(this)}/> 
-      </p>
-
-      <button onClick={() => this.newGame()}>
-       New Game
-      </button>
-      <button onClick={() => this.newOneMoveGame()}>
-       New One Move Game
-      </button>
-      <hr/>
-      {past_guesses}
-      <hr/>
-      <NextGuessRow
-       key={"next"}
-       game={this.state.game}
-       auto={this.getAutoGuess.bind(this)}
-       onSubmit={this.handleNewGuess.bind(this)}     
-       guess={this.state.firstguess}
-       />
+        <button onClick={() => this.newGame()}>New Game</button>
+        <button onClick={() => this.newOneMoveGame()}>New One Move Game</button>
+        <hr />
+        {past_guesses}
+        <hr />
+        <NextGuessRow
+          key={"next"}
+          game={this.state.game}
+          auto={this.getAutoGuess.bind(this)}
+          onSubmit={this.handleNewGuess.bind(this)}
+          guess={this.state.firstguess}
+        />
       </div>
     );
   }
@@ -299,7 +337,7 @@ class App extends Component<AppProps, AppState> {
   constructor(props) {
     super(props);
     this.state = {
-      name: 'React',
+      name: "React"
     };
   }
   render() {
@@ -312,4 +350,4 @@ class App extends Component<AppProps, AppState> {
   }
 }
 
-render(<App />, document.getElementById('root'));
+render(<App />, document.getElementById("root"));
--- src/mastermind.tsx
+++ src/mastermind.tsx
@@ -10,7 +10,7 @@ export class Result {
     this.partial = partial;
   }
   equals(other: Result) {
-    return this.exact == other.exact && this.partial == other.partial;
+    return this.exact === other.exact && this.partial === other.partial;
   }
   str() {
     return "" + this.exact + ", " + this.partial;
@@ -60,7 +60,7 @@ export class Player {
 export class Game {
   readonly width: number;
   readonly valid_values: Value[];
-  constructor(width=4, valid_values=["1", "2", "3", "4", "5", "6"]) {
+  constructor(width = 4, valid_values = ["1", "2", "3", "4", "5", "6"]) {
     this.width = width;
     this.valid_values = valid_values;
   }
@@ -78,7 +78,7 @@ export class Game {
     return guess;
   }
   is_valid_guess(guess: Guess) {
-    if (guess.length != this.width) {
+    if (guess.length !== this.width) {
       return false;
     }
     for (let value of guess) {
@@ -110,29 +110,37 @@ export class Game {
     return lst.reduce((a, b) => a + b, 0);
   }
   count(lst, value) {
-    return lst.reduce((a, b) => a + (b == value ? 1 : 0), 0);
+    return lst.reduce((a, b) => a + (b === value ? 1 : 0), 0);
   }
   // https://gist.github.com/renaudtertrais/25fc5a2e64fe5d0e86894094c6989e10
   zip(arr, ...arrs) {
     return arr.map((val, i) => arrs.reduce((a, arr) => [...a, arr[i]], [val]));
   }
-  // 
+  //
   // https://stackoverflow.com/questions/22015684/how-do-i-zip-two-arrays-in-javascript
   //zip(arr1, arr2) {
   //  return arr1.map((elt, i) => [elt, arr2[i]]);
   //}
   // https://stackoverflow.com/questions/1960473/get-all-unique-values-in-a-javascript-array-remove-duplicates
-  uniq(value, index, arr) { 
+  uniq(value, index, arr) {
     return arr.indexOf(value) === index;
   }
   check_guess(secret: Guess, guess: Guess) {
-    let exact = this.zip(secret, guess).reduce((sum, elt) => sum + (elt[0] == elt[1] ? 1 : 0), 0);
-    let partial = guess.filter(this.uniq).reduce(
-      (sum, val) => sum + Math.min(this.count(secret, val), this.count(guess, val)), 0);
+    let exact = this.zip(secret, guess).reduce(
+      (sum, elt) => sum + (elt[0] === elt[1] ? 1 : 0),
+      0
+    );
+    let partial = guess
+      .filter(this.uniq)
+      .reduce(
+        (sum, val) =>
+          sum + Math.min(this.count(secret, val), this.count(guess, val)),
+        0
+      );
     return new Result(exact, partial - exact);
   }
   is_correct(result: Result) {
-    return result.exact == this.width;
+    return result.exact === this.width;
   }
   guess_string(guess: Guess) {
     return guess.join("");
@@ -169,7 +177,7 @@ function* AllValuesIterator(valid_values: Value[], width: number) {
     indexes.push(0);
     values.push(valid_values[0]);
   }
-  while(true) {
+  while (true) {
     yield values.slice();
     indexes[0] += 1;
     for (let place = 0; place < indexes.length; place++) {
@@ -189,14 +197,14 @@ function* AllValuesIterator(valid_values: Value[], width: number) {
 function* PossibleValuesIterator(game: Game, history: History) {
   let itr = AllValuesIterator(game.valid_values, game.width);
   let next = itr.next();
-  while(!next.done) {
+  while (!next.done) {
     let guess = next.value;
     let match = true;
     for (let hist of history) {
       let result = game.check_guess(hist[0], guess);
       if (!result.equals(hist[1])) {
-          match = false;
-          break;
+        match = false;
+        break;
       }
     }
     if (match) {
@@ -221,11 +229,11 @@ export class AutoPlayer extends Player {
     if (hist1 == null || hist2 == null) {
       return false;
     }
-    if (hist1.length != hist2.length) {
+    if (hist1.length !== hist2.length) {
       return false;
     }
     for (let ii = 0; ii < hist1.length; ii++) {
-      if (hist1[ii] != hist2[ii]) {
+      if (hist1[ii] !== hist2[ii]) {
         return false;
       }
     }
@@ -233,23 +241,25 @@ export class AutoPlayer extends Player {
   }
   is_possible_guesses_computed(history: History) {
     return this.history_matches(
-      history, this._history_for_cached_possible_guesses);
+      history,
+      this._history_for_cached_possible_guesses
+    );
   }
   get_possible_guesses(history: History) {
     if (!this.is_possible_guesses_computed(history)) {
-        this._cached_possible_guesses = [];
-        // TODO: This always recomputes possible values from
-        // scratch, but if the _cached_possible_guesses already
-        // satisfy all the history except the last element,
-        // then we only need to throw out the ones that don't
-        // match the last element of history.
-        let itr = PossibleValuesIterator(this.game, history);
-        let next = itr.next();
-        while(!next.done) {
-          this._cached_possible_guesses.push(next.value);
-          next = itr.next();
-        }
-        this._history_for_cached_possible_guesses = history.slice();
+      this._cached_possible_guesses = [];
+      // TODO: This always recomputes possible values from
+      // scratch, but if the _cached_possible_guesses already
+      // satisfy all the history except the last element,
+      // then we only need to throw out the ones that don't
+      // match the last element of history.
+      let itr = PossibleValuesIterator(this.game, history);
+      let next = itr.next();
+      while (!next.done) {
+        this._cached_possible_guesses.push(next.value);
+        next = itr.next();
+      }
+      this._history_for_cached_possible_guesses = history.slice();
     }
     return this._cached_possible_guesses;
   }
@@ -266,7 +276,7 @@ export class AutoPlayer extends Player {
   make_random_guess(history: History) {
     return this.game.random_choice(this.get_possible_guesses(history));
   }
-  get_guess(num_tries=4, history: History) {
+  get_guess(num_tries = 4, history: History) {
     let guess = this.make_guess(history);
     output("Guessed: " + this.game.guess_string(guess));
     return guess;
@@ -284,11 +294,11 @@ export class RandomPlayer extends AutoPlayer {
 // -------------------------------------------------------------- //
 
 export class MaybeAutoPlayer extends AutoPlayer {
-  get_guess(num_tries=4, history: History) {
+  get_guess(num_tries = 4, history: History) {
     return Player.prototype.get_guess.call(this, num_tries, history);
   }
   get_input(msg: string, history: History) {
-    let guess = Player.prototype.get_input.call(this, msg, history)
+    let guess = Player.prototype.get_input.call(this, msg, history);
     if (this.game.is_correct(this.game.check_guess(guess, "auto".split("")))) {
       return AutoPlayer.prototype.get_guess.call(this, 0, history);
     } else {
